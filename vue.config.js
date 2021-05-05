af2aeefee42699ff6e5e995b5c54c42e5c9b2ad6
tgt <2311965299@qq.com>
2021-05-05 20:39:07 +0800

    the second commit

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,9 @@ module.exports = {
             }
         }
     },
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-site/'
+    : '/',
 
     // 通过 configureWebpack 选项，可对 webpack 进行额外的配置
     // 该配置最终会和 vue-cli 的默认配置进行合并（webpack-merge）
